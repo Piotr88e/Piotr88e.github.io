d7fa2d6dc68e1046433cbfe5c84ba2f3ae1ea2a8
ProfessionalDebil <71475850+ProfessionalDebil@users.noreply.github.com>
2020-12-21 15:13:54 +0700

    Update main page.js

--- assets/js/main page.js
+++ assets/js/main page.js
@@ -1,1 +1,5 @@
-
+document.getElementById("cookbook button").onclick = function() {
+    document.getElementById("test").style.color = "red";
+    document.getElemenyById("test").style.textSize = "40px";
+    document.getElementById("go away").remove();
+}
