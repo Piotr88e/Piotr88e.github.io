eebcabc9ef69b5799be2771450cc94620adabe1c
ProfessionalDebil <71475850+ProfessionalDebil@users.noreply.github.com>
2020-12-21 16:04:02 +0700

    Update main page.js

--- assets/js/main page.js
+++ assets/js/main page.js
@@ -1,5 +1,8 @@
 document.getElementById("cookbook button").onclick = function() {
     document.getElementById("test").style.color = "red";
+    console.log("This probably gonna prints out")
     document.getElemenyById("test").style.textSize = "100px";
+    console.log("Almost there")
     document.getElementById("go away").remove();
+    console.log("The text don't go away? It means you fucked up")
 }
