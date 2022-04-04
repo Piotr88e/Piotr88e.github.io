faf586f921ba0f27be56f4e1b5f7a3e47ec8a4a6
Piotr88e <59716898+Piotr88e@users.noreply.github.com>
2022-04-04 21:15:57 +0200

    Delete assets directory

--- assets/images/hmm.py
+++
@@ -1,1 +0,0 @@
-print("Nothing, this is just for holding the folder so it stays there")
--- assets/js/main page.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("test").onclick = function() {
-    document.getElementById("test").style.color = "red";
-    console.log("This probably gonna prints out");
-    document.getElementById("test").style.textSize = "100px";
-    console.log("Almost there");
-    document.getElementById("go away").remove();
-    console.log("The text don't go away? It means you fucked up");
-    console.log("If this shows up, congrats, you're a master at JavaScript now");
-}
